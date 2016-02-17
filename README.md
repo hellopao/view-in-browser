f00ef4a739db2927ee289a867e294817a097510d
qinjia <qinjia@outlook.com>
2016-02-17 14:31:15 +0800

    upload files

---
+++ README.md
@@ -0,0 +1,11 @@
+## View In Browser
+view a html file in system's default browser
+
+## usage 
+
+### command
+    1. Open the command list (Press F1 or Ctrl + Shift + P)
+    2. Select `View In Browser`
+
+### keybindings
+    1. Press Ctrl + F1
---
+++ src/extension.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from "path";
+
+const open = require('open');
+
+export function activate(context: vscode.ExtensionContext) {
+    
+	let disposable = vscode.commands.registerCommand('extension.viewInBrowser', () => {
+        const file = vscode.window.activeTextEditor.document.fileName;
+        
+        const ext = path.extname(file);
+        
+        if (/\.(html|htm|shtml|xhtml)/.test(ext)) {
+            open(`file:///${file}`);
+        }
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
