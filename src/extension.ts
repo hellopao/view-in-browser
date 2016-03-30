2538bbd07ecb42fb92e86ec80f651fa13bf12b1d
hellopao <qinjia@outlook.com>
2016-03-30 09:58:36 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -6,13 +6,19 @@ import * as path from "path";
 const open = require('open');
 
 export function activate(context: vscode.ExtensionContext) {
-    
+        
 	let disposable = vscode.commands.registerCommand('extension.viewInBrowser', () => {
-        const file = vscode.window.activeTextEditor.document.fileName;
+        var editor = vscode.window.activeTextEditor;
+        
+        if (!editor) {
+            vscode.window.showWarningMessage('no active text editor!');
+            return; 
+        }
+        const file = editor.document.fileName;
         
         const ext = path.extname(file);
         
-        if (/\.(html|htm|shtml|xhtml)/.test(ext)) {
+        if (/^\.(html|htm|shtml|xhtml)$/.test(ext)) {
             open(`file:///${file}`);
         } else {
             vscode.window.showInformationMessage('support html file only!');
