da92e19cc9b81d9e940352883ca83e95c9569aa9
mantasmarcinkus <mantas.marcinkus.bio@gmail.com>
2017-01-08 17:05:04 +0000

    Included Mac as seperate platform.

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,39 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import * as path from "path";
+import * as path from 'path';
 
 const open = require('open');
+const open_darwin = require('mac-open');
+
+// decide what os should be used
+// possible node values 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+const platform = process.platform;
+
+// open file in custom browser
+function openInSpecificPlatform(e: string, op: any, customBrowser: string) {
+    customBrowser ? op(e, customBrowser) : op(e);
+}
 
 // common function for file opening
 function openFile(e: string, customBrowser: string) {
+    // check if it is html file
     const ext = path.extname(e.toString());
     if (/^\.(html|htm|shtml|xhtml)$/.test(ext)) {
-        customBrowser ? open(e, customBrowser) : open(e);
+        // platform is operational system
+        // darwin - mac os, others are good with open npm module
+        if (platform === 'darwin') {
+            openInSpecificPlatform(e, open_darwin, customBrowser);
+        }
+        else {
+            openInSpecificPlatform(e, open, customBrowser);
+        }
     } else {
         vscode.window.showInformationMessage('Supports html file only!');
     }
 }
 
+// main code of the extension
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.viewInBrowser', (e: vscode.Uri) => {
         let config = vscode.workspace.getConfiguration('view-in-browser');
