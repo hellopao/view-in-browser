9a4ab8abaf124387458a07b99a731b99467add43
mantasmarcinkus <mantas.marcinkus.bio@gmail.com>
2017-01-08 16:28:31 +0000

    Added context menu and configuration value to setup what browser should extension use.

--- src/extension.ts
+++ src/extension.ts
@@ -5,27 +5,38 @@ import * as path from "path";
 
 const open = require('open');
 
-export function activate(context: vscode.ExtensionContext) {
-        
-	let disposable = vscode.commands.registerCommand('extension.viewInBrowser', () => {
-        var editor = vscode.window.activeTextEditor;
-        
-        if (!editor) {
-            vscode.window.showWarningMessage('no active text editor!');
-            return; 
-        }
-        const file = editor.document.fileName;
-        
-        const ext = path.extname(file);
-        
-        if (/^\.(html|htm|shtml|xhtml)$/.test(ext)) {
-            open(`file:///${file}`);
-        } else {
-            vscode.window.showInformationMessage('support html file only!');
-        }
-	});
+// common function for file opening
+function openFile(e: string, customBrowser: string) {
+    const ext = path.extname(e.toString());
+    if (/^\.(html|htm|shtml|xhtml)$/.test(ext)) {
+        customBrowser ? open(e, customBrowser) : open(e);
+    } else {
+        vscode.window.showInformationMessage('Supports html file only!');
+    }
+}
 
-	context.subscriptions.push(disposable);
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.viewInBrowser', (e: vscode.Uri) => {
+        let config = vscode.workspace.getConfiguration('view-in-browser');
+        let customBrowser = config.get<string>("customBrowser");
+
+        // if there is Uri it means the file was selected in the explorer.
+        if (e.path) {
+            openFile(e.fsPath, customBrowser);
+        }
+        else {
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                vscode.window.showWarningMessage('No active text editor found!');
+                return;
+            }
+
+            const file = editor.document.fileName;
+            openFile(`file:///${file}`, customBrowser);
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
